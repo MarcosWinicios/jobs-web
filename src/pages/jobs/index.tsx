fdde74a34f840e66bb01c14f66e6751a349f9508
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-10-25 20:23:47 -0300

    created presentation  section

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa"
 import Header from "../../components/Header";
 import "./styles.css";
 const Jobs = () => {
@@ -6,62 +7,54 @@ const Jobs = () => {
         <>
             <Header />
             <main>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
+                <section className="presentation">
+                    <div>
+                        <h1>Vagas para Programadores e Desenvolvedores</h1>
+                        <p>Domina alguma linguagem de programação (Front-End, Back-End, Mobile ou Full Stack)?<br />Quer ser contratado como freelancer (PJ), estagiário ou CLT?<br />Então você está no lugar certo!</p>
+                        <div className="search-group">
+                            <div>
+                                <FaSearch color="#000" />
+                            </div>
+                            <select name="select-skills" id="skills-select">
+                                <option value="Todos os Skills">Todos os Skills</option>
+                                <option value="Todos os Skills">Front-End</option>
+                                <option value="Todos os Skills">Back-End</option>
+                                <option value="Todos os Skills">Java</option>
+                                <option value="Todos os Skills">HTML</option>
+                                <option value="Todos os Skills">CSS</option>
+                                <option value="Todos os Skills">MYSQL</option>
+                                <option value="Todos os Skills">REACT JS</option>
 
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
+                                <option value="Todos os Skills">Todos os Skills</option>
+                                <option value="Todos os Skills">Front-End</option>
+                                <option value="Todos os Skills">Back-End</option>
+                                <option value="Todos os Skills">Java</option>
+                                <option value="Todos os Skills">HTML</option>
+                                <option value="Todos os Skills">CSS</option>
+                                <option value="Todos os Skills">MYSQL</option>
+                                <option value="Todos os Skills">REACT JS</option>
 
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
+                                <option value="Todos os Skills">Todos os Skills</option>
+                                <option value="Todos os Skills">Front-End</option>
+                                <option value="Todos os Skills">Back-End</option>
+                                <option value="Todos os Skills">Java</option>
+                                <option value="Todos os Skills">HTML</option>
+                                <option value="Todos os Skills">CSS</option>
+                                <option value="Todos os Skills">MYSQL</option>
+                                <option value="Todos os Skills">REACT JS</option>
 
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
-                <p>Teste</p>
+                                <option value="Todos os Skills">Todos os Skills</option>
+                                <option value="Todos os Skills">Front-End</option>
+                                <option value="Todos os Skills">Back-End</option>
+                                <option value="Todos os Skills">Java</option>
+                                <option value="Todos os Skills">HTML</option>
+                                <option value="Todos os Skills">CSS</option>
+                                <option value="Todos os Skills">MYSQL</option>
+                                <option value="Todos os Skills">REACT JS</option>
+                            </select>
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     );
