dc1f4ffa5292a8156073178c3b2657805a6be5d3
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-10-26 22:48:09 -0300

    position image

--- src/components/Job/index.tsx
+++ src/components/Job/index.tsx
@@ -1,50 +1,56 @@
 import React from "react";
 import { FaBriefcase, FaMapMarkerAlt, FaBuilding, FaChartBar, FaFileAlt, FaPlane } from "react-icons/fa";
 import "../Job/styles.css";
+import CompanyImage from "../../assets/images/company-image.png";
 
 const Job = () => {
     return (
         <div className="job-container">
-            <h3>Desenvolvedor(a) de Sofware Pleno</h3>
-            <div className="items-job">
-                <span className="company">
-                    <FaBriefcase />
-                    Deep Seed Solutions
-                </span>
-                <span className="location">
-                    <FaMapMarkerAlt />
-                    Rio de Janeiro, Rio de Janeiro, Brasil
-                </span>
-                <span className="company-size">
-                    <FaBuilding />
-                    Pequena/média empresa
-                </span>
-                <span className="experience-level">
-                    <FaChartBar />
-                    Pleno
-                </span>
-                <span className="contract-type">
-                    <FaFileAlt />
-                    CLT
-                </span>
-                <span className="accept-outside">
-                    <FaPlane />
-                    Aceita de fora
-                </span>
-            </div>
-            <div className="list-tag-skills">
-                <span className="tag-skill">
-                    C++
-                </span>
-                <span className="tag-skill">
-                    Git
-                </span>
-                <span className="tag-skill">
-                    MySQL
-                </span>
-                <span className="tag-skill">
-                    POO
-                </span>
+            <figure>
+                <img src={CompanyImage} alt="Company Image" loading="lazy" width="100" height="100" />
+            </figure>
+            <div className="container-data-job">
+                <h3>Desenvolvedor(a) de Sofware Pleno</h3>
+                <div className="items-job">
+                    <span className="company">
+                        <FaBriefcase />
+                        Deep Seed Solutions
+                    </span>
+                    <span className="location">
+                        <FaMapMarkerAlt />
+                        Rio de Janeiro, Rio de Janeiro, Brasil
+                    </span>
+                    <span className="company-size">
+                        <FaBuilding />
+                        Pequena/média empresa
+                    </span>
+                    <span className="experience-level">
+                        <FaChartBar />
+                        Pleno
+                    </span>
+                    <span className="contract-type">
+                        <FaFileAlt />
+                        CLT
+                    </span>
+                    <span className="accept-outside">
+                        <FaPlane />
+                        Aceita de fora
+                    </span>
+                </div>
+                <div className="list-tag-skills">
+                    <span className="tag-skill">
+                        C++
+                    </span>
+                    <span className="tag-skill">
+                        Git
+                    </span>
+                    <span className="tag-skill">
+                        MySQL
+                    </span>
+                    <span className="tag-skill">
+                        POO
+                    </span>
+                </div>
             </div>
         </div>
     );
