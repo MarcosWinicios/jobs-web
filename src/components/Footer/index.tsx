e94cf5dba6e39672c8f18fd9c6eb8fb26b03ad62
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-03 13:14:37 -0300

    position footer links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,52 +4,57 @@ import "./styles.css";
 const Footer = () => {
     return (
         <footer>
-            <section className="footer-navigation">
-                <div className="links-container">
-                    <h3 className="footer-title">Título</h3>
-                    <nav>
-                        <ul>
-                            <li><a href="https://jobsdev.netlify.app/">Sobre nós</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Contratar desenvolvedor</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Salário programador</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Blog de programação</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Parceiros</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Perguntas frequentes</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Termos</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Contatos</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <div className="links-container">
-                    <h3 className="footer-title">Vagas</h3>
-                    <nav>
-                        <ul>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas desenvolvedor</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador JAVA</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador PHP</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador C#</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador NODE.JS</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador FRONT-END</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador .NET</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador ANGULAR</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <div className="links-container">
-                    <nav>
-                        <ul>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador JAVASCRIPT</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador REACT</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador ANDROID</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador IOS</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador RUBY ON RAILS</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas programador PYTHON</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas remotas programação</a></li>
-                            <li><a href="https://jobsdev.netlify.app/">Vagas arquivadas programação</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </section>
+            <main>
+
+                <section className="footer-navigation">
+                    <div className="links-container">
+                        <h3 className="footer-title">Título</h3>
+                        <nav>
+                            <ul>
+                                <li><a href="https://jobsdev.netlify.app/">Sobre nós</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Contratar desenvolvedor</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Salário programador</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Blog de programação</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Parceiros</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Perguntas frequentes</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Termos</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Contatos</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="links-container">
+                        <h3 className="footer-title">Vagas</h3>
+                        <nav>
+                            <ul>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas desenvolvedor</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador JAVA</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador PHP</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador C#</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador NODE.JS</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador FRONT-END</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador .NET</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador ANGULAR</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="links-container">
+                        <nav>
+                            <br />
+                            <ul>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador JAVASCRIPT</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador REACT</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador ANDROID</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador IOS</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador RUBY ON RAILS</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas programador PYTHON</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas remotas programação</a></li>
+                                <li><a href="https://jobsdev.netlify.app/">Vagas arquivadas programação</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </section>
+            </main>
+
         </footer>
     );
 }
